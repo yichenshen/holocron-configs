e04e00b8de0e096c522ff6cb4561aac1dbe4ede2
Shen Yichen <2007.yichen@gmail.com>
2016-12-15 00:35:08 +0800

    Use archive instead of cloning
    git repo contains a 1GB+ history that we do not need.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- fonts/clone-fonts.sh
+++ fonts/clone-fonts.sh
@@ -2,6 +2,12 @@
 
 git clone https://github.com/google/fonts ~/.fonts/google-fonts
 
-git clone https://github.com/googlei18n/noto-cjk.git ~/.fonts/noto-cjk
+wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip
+
+mkdir -p ~/.font/NotoSansCJK
+
+unzip NotoSansCJK.ttc.zip -d ~/.font/NotoSansCJK
+
+rm -f NotoSansCJK.ttc.zip
 
 fc-cache -fv
