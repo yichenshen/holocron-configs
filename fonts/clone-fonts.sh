37a44f7117c97abaaf35de59257593e6bf082230
Shen Yichen <2007.yichen@gmail.com>
2018-11-12 23:19:20 -0800

    Use shallow clone for fonts
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- fonts/clone-fonts.sh
+++ fonts/clone-fonts.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/google/fonts ~/.fonts/google-fonts
-
-wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip
-mkdir -p ~/.fonts/NotoSansCJK
-unzip NotoSansCJK.ttc.zip -d ~/.fonts/NotoSansCJK
-rm -f NotoSansCJK.ttc.zip
-
-wget https://noto-website.storage.googleapis.com/pkgs/NotoSerifCJK.ttc.zip
-mkdir -p ~/.fonts/NotoSerifCJK
-unzip NotoSerifCJK.ttc.zip -d ~/.fonts/NotoSerifCJK
-rm -f NotoSerifCJK.ttc.zip
+git clone --depth 1 https://github.com/google/fonts ~/.fonts/google-fonts
+git clone --depth 1 https://github.com/googlei18n/noto-cjk.git ~/.fonts/NotoCJK
 
 fc-cache -fv
