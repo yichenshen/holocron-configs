f1f3451a0ddca6364de9a34a6a43f1bce4013b65
Shen Yichen <2007.yichen@gmail.com>
2024-01-15 22:45:10 -0800

    [fish] Change fish plugin file
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- fish/install.sh
+++ fish/install.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/fish/conf.d
 ln -s -f $(git rev-parse --show-toplevel)/fish/functions/*.fish ~/.config/fish/functions/
 ln -s -f $(git rev-parse --show-toplevel)/fish/conf.d/*.fish ~/.config/fish/conf.d/
-ln -s -f $(git rev-parse --show-toplevel)/fish/fishfile ~/.config/fish/
+ln -s -f $(git rev-parse --show-toplevel)/fish/fish_plugins ~/.config/fish/
 ln -s -f $(git rev-parse --show-toplevel)/fish/fish_colors ~/.config/fish/
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
